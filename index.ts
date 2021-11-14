351f4111a3ea6a5b12b36dc1a91e5731190987fa
Lucas Sandoval <sandovalucas88@gmail.com>
2021-11-14 12:01:59 -0300

    Creamos las implementaciones de 'Moneda' que son 'PesoArgentino' y 'PesoUruguayo'

--- index.ts
+++ index.ts
@@ -2,3 +2,20 @@ interface Moneda {
     valor_dolar: number;
     nombre_moneda: string;
 }
+//---------------- monedas ---------------
+class PesoArgentino implements Moneda {
+    valor_dolar: number;
+    nombre_moneda: string;
+    constructor(valor_dolar:number, nombre_moneda:string){
+        this.valor_dolar = valor_dolar;
+        this.nombre_moneda = nombre_moneda;
+    }
+}
+class PesoUruguayo implements Moneda {
+    valor_dolar: number;    
+    nombre_moneda: string;
+    constructor(valor_dolar:number, nombre_moneda:string){
+        this.valor_dolar = valor_dolar;
+        this.nombre_moneda = nombre_moneda;
+    }
+}
